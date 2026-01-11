ba15ba9d6361c12d1ca5d1322de561f7f8d769f7
elanrrrk <elanara2000@gmail.com>
2026-01-11 17:56:14 +0300

    1

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Игнорируем ошибки типов и линтера при сборке, чтобы деплой не падал
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;
